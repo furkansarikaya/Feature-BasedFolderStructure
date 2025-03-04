63e1be66e265751ec1a00e569a3aa52797e5d1fc
furkansarikaya <furkannsarikaya@gmail.com>
2025-03-04 20:57:22 +0300

    removed to IAuthorizable.cs

--- src/FeatureBasedFolderStructure.Application/Common/Interfaces/IAuthorizable.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.Claims;
-
-namespace FeatureBasedFolderStructure.Application.Common.Interfaces;
-
-public interface IAuthorizable 
-{
-    Task<bool> AuthorizeAsync(ClaimsPrincipal user);
-}
